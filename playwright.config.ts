bc3fea3d42500c2b23ab0569480808a3a06bcd4b
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2026-06-20 17:09:32 +0200

    Update webServer {port} to {url} with localhost

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,7 +2,7 @@ import {defineConfig, devices} from '@playwright/test';
 
 export default defineConfig({
   webServer: [
-    {url: 'http://127.0.0.1:4100/', command: 'pnpm vite preview --port 4100'},
-    {url: 'http://127.0.0.1:4300/', command: 'go run .'},
+    {url: 'http://localhost:4100/', command: 'pnpm vite preview --port 4100'},
+    {url: 'http://localhost:4300/', command: 'go run .'},
   ],
 });
