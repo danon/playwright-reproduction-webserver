53b810690e94653ef08e259755eb787d663372a4
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2026-06-23 08:51:33 +0200

    Add {stdout:'pipe',stderr:'pipe'} to webServer commands

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,7 +2,17 @@ import {defineConfig, devices} from '@playwright/test';
 
 export default defineConfig({
   webServer: [
-    {url: 'http://localhost:4100/', command: 'pnpm vite preview --port 4100'},
-    {url: 'http://localhost:4300/', command: 'go run .'},
+    {
+      url: 'http://localhost:4100/',
+      command: 'pnpm vite preview --port 4100',
+      stdout: 'pipe',
+      stderr: 'pipe',
+    },
+    {
+      url: 'http://localhost:4300/',
+      command: 'go run .',
+      stdout: 'pipe',
+      stderr: 'pipe',
+    },
   ],
 });
